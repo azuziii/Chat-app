b7b3cbeaf4d186b391e5be1bfa9fe915c2733af1
Azuzi <azuzii@proton.me>
2024-09-21 22:05:22 +0100

    Created user-mini-row component

---
+++ src/app/core/components/user/ui/user-mini-row/user-mini-row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-mini-row',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-mini-row.component.html',
+  styleUrl: './user-mini-row.component.css'
+})
+export class UserMiniRowComponent {
+
+}
