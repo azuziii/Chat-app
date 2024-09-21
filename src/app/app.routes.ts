bf623fcd3ac8a5483a689fa0a1d49cda3825233f
Azuzi <azuzii@proton.me>
2024-09-21 21:36:31 +0100

    Added home route and register route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { authGuard } from './core/guard/auth.guard';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
+  {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
