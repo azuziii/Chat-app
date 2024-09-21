111cc1db08be6a94f3da6ae325afc27343683f17
Azuzi <azuzii@proton.me>
2024-09-21 21:36:46 +0100

    Created authGuard

---
+++ src/app/core/guard/auth.guard.ts
@@ -0,0 +1,9 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  console.log('guard req init');
+  return auth.isAuthenticated();
+};
