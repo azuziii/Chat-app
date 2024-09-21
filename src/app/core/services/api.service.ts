a7005517a80590f3488d713cfca987f3388613cb
Azuzi <azuzii@proton.me>
2024-09-21 20:24:06 +0100

    Created API service

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  API_URL = 'http://localhost:3000/';
+
+  get(endpoint = ''): Observable<any> {
+    return this.http.get(this.API_URL + endpoint);
+  }
+
+  post(endpoint = '', body: any = {}) {
+    return this.http.post(this.API_URL + endpoint, body);
+  }
+}
