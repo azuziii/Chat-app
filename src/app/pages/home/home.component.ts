4f404c64e22dc536e328beb91580c8ecee5d46e2
Azuzi <azuzii@proton.me>
2024-09-22 15:24:26 +0100

    Imported DialogAddFriendComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { SideBarComponent } from '../../core/components/side-bar/side-bar.component';
 import { ContactsComponent } from '../../core/components/contacts/contacts.component';
 import { ChatComponent } from '../../core/components/chat/chat.component';
+import { DialogAddFriendComponent } from '../../core/components/dialogs/add-friend/add-friend.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SideBarComponent, ContactsComponent, ChatComponent],
+  imports: [
+    SideBarComponent,
+    ContactsComponent,
+    ChatComponent,
+    DialogAddFriendComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
