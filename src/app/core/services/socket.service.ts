c5d1a78d75d85dddd0ebed228ffdbfff1025fcfd
Azuzi <azuzii@proton.me>
2024-09-23 00:26:51 +0100

    Created socketService

---
+++ src/app/core/services/socket.service.ts
@@ -0,0 +1,32 @@
+import { ApplicationRef, inject, Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { StorageService } from './storage.service';
+import { first } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  socket: Socket;
+  constructor(private storage: StorageService) {
+    this.socket = io('http://localhost:3002', {
+      autoConnect: false,
+      query: {
+        access_token: this.storage.get('access_token'),
+      },
+    });
+    inject(ApplicationRef)
+      .isStable.pipe(first((isStable) => isStable))
+      .subscribe(() => {
+        this.socket.connect();
+      });
+  }
+
+  emit(event: string, data: any = {}) {
+    this.socket.emit(event, data);
+  }
+
+  on(event: string, callback: (...args: any[]) => void) {
+    this.socket.on(event, callback);
+  }
+}
