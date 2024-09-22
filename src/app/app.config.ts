f25c495a8d68b2ed9438ee5009c15bf0c70b005f
Azuzi <azuzii@proton.me>
2024-09-23 00:12:17 +0100

    provided socketConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { routes } from './app.routes';
 import {
   HTTP_INTERCEPTORS,
@@ -15,9 +15,17 @@ import {
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+const socketConfig: SocketIoConfig = {
+  url: 'http://localhost:3002',
+  options: {
+    withCredentials: true,
+  },
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(SocketIoModule.forRoot(socketConfig)),
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
